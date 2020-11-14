279fa9f4c051065283b51b229ef0bccb9671722d
Dawid Janiszewski <dawiz147@gmail.com>
2020-11-14 15:20:01 +0100

    Initialize

---
+++ Radiowe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Radiowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

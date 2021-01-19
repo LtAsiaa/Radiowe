22ace9e0365f44f06633155b2c9ac126e156458a
Asia Szewczyk <asiaszewczyk73@gmail.com>
2021-01-19 20:54:54 +0100

    .

---
+++ Radio/Radio/Models/AddStation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Radio.Models
+{
+    public class AddStation
+    {
+        public string nazwa { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+        public double moc { get; set; }
+        public double zysk { get; set; }
+        public int nrkanalu { get; set; }
+        /*public AddStation(string nazwa, int x, int y, double moc, double zysk, int nrkanalu)
+        {
+            this.nazwa = nazwa;
+            this.x = x;
+            this.y = y;
+            this.moc = moc;
+            this.zysk = zysk;
+            this.nrkanalu = nrkanalu;
+        }*/
+    }
+}
--- Radio/Radio/Pages/Index1.cshtml.cs
+++ Radio/Radio/Pages/Index1.cshtml.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Radio.Models;
 
 namespace Radio.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public AddStation Stacja { get; set; }
+        
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -21,14 +26,16 @@ namespace Radio.Pages
         {
 
         }
-        public void OnPost()
+        public IActionResult OnPost()
         {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
 
+            return RedirectToPage("./Index1");
         }
 
-        protected virtual void btnLogin_Click1(EventArgs e)
-        {
-
-        }
+        
     }
 }

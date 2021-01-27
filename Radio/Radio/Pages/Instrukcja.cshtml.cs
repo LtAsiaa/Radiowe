d894037f1679f2e02673936ddd5d9baea3e90db9
Asia Szewczyk <asiaszewczyk73@gmail.com>
2021-01-27 14:45:12 +0100

    instrukcja

---
+++ Radio/Radio/Pages/Instrukcja.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Radio.Models;
+using System.Data;
+
+namespace Radio.Pages
+{
+    public class InstrukcjaModel : PageModel
+    {
+
+        public void OnGet()
+        {
+
+        }
+
+
+    }
+}
